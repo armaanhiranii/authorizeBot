3412bfb8240aa445da4243e5d681fc491e56b046
armaanhiranii <armaanhirani@ucsb.edu>
2022-07-29 17:45:45 -0700

    Updated Driver File and Reformatting Dataframe

--- driver.py
+++ driver.py
@@ -1,7 +1,11 @@
 from selenium_bot import authorize_bot
 from get_text_file import get_text
+from text_to_excel import text_to_funstuff
 
-if __name__ == "__main__":
+def main():
     authorize_bot()
-    print(get_text())
-    print('done')
+    text_to_funstuff(get_text(), r"C:\Users\conf.asst5\Desktop\test.xlsx")
+
+    print("Excel File has been written!")
+    
+main()
